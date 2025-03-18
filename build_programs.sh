c05c6f3ddefd16bdd046c1ec63cf3da0e9aac4ed
Tripplenut <afabius05@gmail.com>
2025-03-18 16:54:18 -0400

    elf2sun in all its glory

--- build_programs.sh
+++ build_programs.sh
@@ -5,7 +5,7 @@
 for file in programs/*.c; do
   [ -f "$file" ] || continue
   filename=$(basename -- "$file" .c)
-  gcc "$file" -o "build/$filename.out" -Wall -Wextra -m32 -static -nostdlib
+  gcc "$file" -T linker.ld -o "build/$filename.out" -Wall -Wextra -m32 -static -nostdlib -Wl,--build-id=none
 done
 
 # To build in 32 bit you need multilib gcc
