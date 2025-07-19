72570d48c2a23411c721b7825c90d6743318cf30
Anthony Fabius <afabius05@gmail.com>
2025-07-19 15:34:24 -0700

    Updated example build script
    With the addition of libp a new way of building paradise programs exists requiring an update to the example build script. The script now includes an example of how to compile and link user programs that decide to use the library.

--- programs/example_build.sh
+++ programs/example_build.sh
@@ -2,4 +2,8 @@
 # Here is an example of the flags needed to build a file main.c in a program folder called example
 gcc main.c -T ../linker.ld -o ../../build/example.out -Wall -Wextra -fno-pic -fno-asynchronous-unwind-tables -m32 -static -nostdlib -Wl,--build-id=none
 
-# Ensure that you use the provided linker and the exe is put in build/
+# Here is an example of the building that same file but using the ParadiseOS Standard Library in the program
+gcc -c main.c -o main.o -Wall -Wextra -fno-pic -fno-asynchronous-unwind-tables -m32 -masm=intel -mpreferred-stack-boundary=2
+gcc main.o ../libp.o -T ../linker.ld -o ../../build/example.out -m32 -static -nostdlib -Wl,--build-id=none
+
+# Ensure that you use the provided linker, libp.o and put the exe in build/
